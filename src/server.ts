0bb50d3393bd773fd7b8aa2da7482c461926d491
Leonardo <leodolago@gmail.com>
2021-06-28 22:31:18 -0300

    Segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,7 @@
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-// rota
-app.get("/test", (request, response) => {
-   return response.send('Olé Mundo');
-})
-
-app.post("/test-post", (req, res) => {
-    return res.send('Olá Mundo Post');
-})
 
 app.listen(3000 , () => console.log('Server is running'));
